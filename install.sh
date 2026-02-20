f76de587f46356539e45ea8d3843aada251ed028
colmor05 <colmor05@gmail.com>
2026-02-20 14:22:04 -0500

    Modified install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Clones the repo for Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Downloads vim if vim is not detected
+if [ ./vim != /usr/bin/ ]; then
+	echo "/usr/bin/vim not found."
+	echo "Downloading vim"
+	sudo apt install vim -y
+
+# Clones the repo for Vundle if it is not detected
+if [ .vim != $HOME/ ]; then
+	echo "Downloading Vundle"
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
 # Copies .vimrc from vim-config to home if .vimrc is not in the home directory
 if [ $HOME != $HOME/.vimrc ]; then
@@ -9,5 +18,6 @@ if [ $HOME != $HOME/.vimrc ]; then
 fi
 
 # Installs the plugins seen at the end of the file
+echo "Applying Plugins"
 vim +PluginInstall +qall
 echo "Done"
